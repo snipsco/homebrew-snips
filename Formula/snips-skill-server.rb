a202572ee117f95dc2405206367b46905f54e088
Kevin Lefèvre <kevin.lefevre@snips.ai>
2018-10-30 10:47:32 +0100

    Add service definition

--- Formula/snips-skill-server.rb
+++ Formula/snips-skill-server.rb
@@ -38,6 +38,41 @@ class SnipsSkillServer < Formula
       zsh_completion.install "#{target_dir}/completion/_snips-skill-server"
     end
   end
+  
+  plist_options :manual => "snips-skill-server -c #{HOMEBREW_PREFIX}/etc/snips.toml"
+  
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/snips-skill-server</string>
+          <string>-c</string>
+          <string>#{etc}/snips.toml</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/snips/snips-skill-server.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/snips/snips-skill-server.log</string>
+        <key>ProcessType</key>
+        <string>Interactive</string>
+      </dict>
+    </plist>
+    EOS
+  end
 
   test do
     assert_equal "snips-skill-server #{version}\n", shell_output("#{bin}/snips-skill-server --version")
