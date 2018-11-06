651f3201f4fbc6e7c13c781fba6d88b6199099ff
Kevin Lefevre <kevin.lefevre@snips.ai>
2018-11-06 11:43:29 +0100

    Fix bump script

--- .ci/bump.sh
+++ .ci/bump.sh
@@ -17,5 +17,5 @@ formula_files=${@:3}
 echo "version: $version, revision: $rev, formula_files: $formula_files"
 
 # Update formulae with new version
-sed -i.bk "s/:tag => \".*\"/:tag => \"$version\"/g" $formula_files
-sed -i.bk "s/:revision => \".*\"/:revision => \"$rev\"/g" $formula_files
+sed -i.bk "s/:tag => \".*\",/:tag => \"$version\",/g" $formula_files
+sed -i.bk "s/:revision => \".*\",/:revision => \"$rev\",/g" $formula_files
